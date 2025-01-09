85091f16968a69c88558f40a0b5e76fd3f842aec
dkaziamov <d.kaziamov@gmail.com>
2025-01-09 19:35:11 +0400

    fixed line length

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -15,32 +15,12 @@ from brain_games.game_utils import (
     welcome_user,
 )
 
-# v 1.0
-# def start_brain_even():
-#     TARGET_SCORE = 3
-#     user_score = 0
-#     user_name = welcome_user()
-#     game_is_active = True
-#     while game_is_active:
-#         print('Answer "yes" if the number is even, otherwise answer "no".')
-#         question_number = generate_random_number()
-#         print(f'Question: {question_number}')
-#         user_answer = prompt.string('Your answer: ')
-#         correct_answer = if_number_is_even(question_number)
-#         if user_answer == correct_answer:
-#             print('Correct!')
-#             user_score += 1
-#             if user_score >= TARGET_SCORE:
-#                 print(f'Congratulations, {user_name}!')
-#                 game_is_active = False
-#         else:
-#             print(f'"{user_answer}" is wrong answer ;(. Correct answer was "{correct_answer}".')
-#             game_is_active = False
-
 
 def start_game(game_function):
-    target_score = 3  # maximum score required to win the game
-    difficulty_level = 10  # sets the maximum number for generate_random_number()
+    # maximum score required to win the game
+    target_score = 3
+    # sets the maximum number for generate_random_number()
+    difficulty_level = 10
     user_score = 0
     user_name = welcome_user()
 
@@ -69,7 +49,9 @@ def brain_calc(difficulty_level, user_name):
     second_number = generate_random_number(difficulty_level)
     math_symbol = generate_random_math_symbol()
     print(f'Question: {first_number} {math_symbol} {second_number}')
-    correct_answer = math_symbol_to_math_operation(math_symbol)(first_number, second_number)
+    correct_answer = math_symbol_to_math_operation(math_symbol)(
+        first_number, second_number
+    )
 
     user_answer = prompt.string('Your answer: ')
     if not is_user_answer_number(user_answer):
