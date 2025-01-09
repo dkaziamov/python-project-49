58cf1198348656844124868f038fb8a464ce64f2
dkaziamov <d.kaziamov@gmail.com>
2025-01-09 19:11:31 +0400

    fixed brain_games.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,7 @@
-# import prompt
-#
-#
-# def welcome_user():
-#     name = prompt.string('May I have your name? ')
-#     print(f"Hello, {name}!")
+import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f"Hello, {user_name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,22 +1,10 @@
-# from random import randint
-#
-#
-# def create_arithmetic_progression():
-#     lenght = randint(5, 10)
-#     start_number = randint(1, 100)
-#     step = randint(2, 25)
-#     list_of_numbers = []
-#     for position in range(lenght):
-#         list_of_numbers.append(start_number)
-#         start_number += step
-#
-#     answer_position = randint(0, lenght - 1)
-#     answer = list_of_numbers[answer_position]
-#     list_of_numbers[answer_position] = '..'
-#
-#     for number in list_of_numbers:
-#         print(number, end=" ")
-#
-#
-# create_arithmetic_progression()
-    
+from brain_games.cli import welcome_user
+
+
+def main():
+    welcome_user()
+
+
+if __name__ == "__main__":
+    main()
+
