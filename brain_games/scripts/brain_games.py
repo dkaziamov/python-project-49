c6e90478f6eb220a7ac273bc1679a1b597d2d8d0
dkaziamov <d.kaziamov@gmail.com>
2025-01-14 14:27:42 +0400

    fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,8 @@
-from brain_games.game_engine import start_game
 from brain_games.cli import welcome_user
 
 
 def main():
-    start_game(brain_gcd)
+    welcome_user()
 
 
 if __name__ == "__main__":
